cb00cdb137f39cee48add9adf92f4149c4955073
Khutlu <Khutlu@KHULTHU>
2023-04-02 20:54:30 -0300

    Agregar archivos de proyecto.

---
+++ Api-Agendate-App/Connection/ConnectionBD.cs
@@ -0,0 +1,18 @@
+﻿namespace Api_Agendate_App.Connection
+{
+    public class ConnectionBD
+    {
+        private string connectionString;
+
+        public ConnectionBD ()
+        {
+            var appSettings = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            connectionString = appSettings.GetSection("ConnectionStrings:masterconnection").Value;
+        }
+
+        public string ConnectionString ()
+        {
+            return connectionString;
+        }
+    }
+}

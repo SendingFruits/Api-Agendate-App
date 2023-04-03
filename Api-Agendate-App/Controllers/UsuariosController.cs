161d1ffb446ff4997215e7f26b083182169c6a2e
Khutlu <kevinmiranda621@gmail.com>
2023-04-02 21:06:13 -0300

    feat: Primeros cambios de Ejemplo para aprender a manejarnos en Git

---
+++ Api-Agendate-App/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿
+using Api_Agendate_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Agendate_App.Controllers
+{
+    [ApiController]
+    [Route("api/obtenerUsuarios")]
+    public class UsuariosController
+    {
+        //[HttpGet]
+        //public async Task <ActionResult<List<MUsuarios>>> GetUsuarios()
+        //{
+
+        //}
+    }
+}

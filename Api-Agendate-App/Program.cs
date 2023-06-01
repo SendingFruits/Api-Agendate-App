7b38e56c4ca1b75ecf71341fb3a8777d30568a4d
Khutlu <kevinmiranda621@gmail.com>
2023-05-31 22:00:51 -0300

    feat: Cambios en el proyecto

--- Api-Agendate-App/Program.cs
+++ Api-Agendate-App/Program.cs
@@ -1,9 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Logic.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+//Configuracion servicios
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var app = builder.Build();
+
+//Injeccion de servicios
+///Aca va el apartado de la carpeta Services
+///
+
+//Configurar el contexto de la base de datos
+builder.Services.AddScoped<DataContext>();
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(DataContext.GetConnectionString());
+});
 
 if (app.Environment.IsDevelopment())
 {

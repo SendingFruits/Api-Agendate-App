d5e6efd8aee0f55ea6d57a6970a5a54cd2287367
Khutlu <kevinmiranda621@gmail.com>
2023-06-07 21:59:02 -0300

    fix: Desvincula modelos y agrega comentario

--- Api-Agendate-App/Program.cs
+++ Api-Agendate-App/Program.cs
@@ -17,8 +17,8 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 var app = builder.Build();
 
-//Para iniciar una base de datos la primera vez que se ejecute el proyecto.
-using (var scope = app.Services.CreateScope())  
+//Para migrar los cambios a una base de datos, la primera vez que se ejecute la API.
+using (var scope = app.Services.CreateScope())
 {
     var Context = scope.ServiceProvider.GetRequiredService<DataContext>();
     Context.Database.Migrate();

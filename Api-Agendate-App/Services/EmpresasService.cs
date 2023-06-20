5726889772981ab891aef7c7862f80cf083a3a27
Khutlu <kevinmiranda621@gmail.com>
2023-06-19 22:52:41 -0300

    feat: StartUp

--- Api-Agendate-App/Services/EmpresasService.cs
+++ Api-Agendate-App/Services/EmpresasService.cs
@@ -1,6 +1,28 @@
-﻿namespace Api_Agendate_App.Services
+﻿using Logic.Entities;
+
+namespace Api_Agendate_App.Services
 {
     public class EmpresasService
     {
+        private readonly Logic.Data.DataContext dataContext;
+
+        public EmpresasService(Logic.Data.DataContext p_dataContext)
+        {
+            dataContext = p_dataContext;
+        }
+
+        public Empresa Create(Empresa nuevaEmpresa)
+        {
+            var empresas = dataContext.Empresas.Where(emp => emp.RutDocumento == nuevaEmpresa.RutDocumento).FirstOrDefault();
+            if (empresas != null)
+            {
+                return null;
+            }
+
+            dataContext.Empresas.Add(nuevaEmpresa);
+            dataContext.SaveChanges();
+
+            return nuevaEmpresa;
+        }
     }
 }

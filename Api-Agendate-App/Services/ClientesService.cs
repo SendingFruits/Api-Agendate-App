994dcb1b32edab337ab8a3f2ea1e8accb9145525
Germ17 <germantexeira@gmail.com>
2023-06-21 18:49:14 -0300

    Logueo

---
+++ Api-Agendate-App/Services/ClientesService.cs
@@ -0,0 +1,26 @@
+﻿using Logic.Data;
+using Logic.Entities;
+
+namespace Api_Agendate_App.Services
+{
+    public class ClientesService
+    {
+        private readonly Logic.Data.DataContext dataContext;
+        public ClientesService(Logic.Data.DataContext p_dataContext)
+        {
+            dataContext = p_dataContext;
+        }
+
+
+        public Cliente Login (string username, string password) 
+        {
+            var clientes = dataContext.Clientes.Where(cli=> cli.NombreUsuario== username&& cli.Contrasenia== password).FirstOrDefault();
+            if (clientes == null)
+            {
+                return null;
+            }
+            return clientes;
+        }
+
+    }
+}

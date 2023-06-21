8e6e9fdb2f1877b53fc274af885d9ff3e376f7f2
Khutlu <kevinmiranda621@gmail.com>
2023-06-21 18:44:02 -0300

    feat: Agrega Respuestas para la API

---
+++ Api-Agendate-App/Constantes/ConstantesDeErrores.cs
@@ -0,0 +1,28 @@
+﻿namespace Api_Agendate_App.Constantes
+{
+    public class ConstantesDeErrores
+    {
+        public const int Exito = 0;
+        public const int ErrorEntidadExistente = 1;
+        public const int ErrorInsertandoEntidad = 2;
+
+        public static string DevolverMensaje(int codigoError)
+        {
+            string mensaje = "";
+            switch (codigoError)
+            {
+                case Exito:
+                    mensaje = "Exito";
+                    break;
+                case ErrorEntidadExistente:
+                    mensaje = "La entidad a crear ya existe en el sistema";
+                    break;
+                case ErrorInsertandoEntidad:
+                    mensaje = "Error insertando la entidad en el sistema";
+                    break;
+            }
+            return mensaje;
+        }
+    }
+
+}
---
+++ Api-Agendate-App/Utilidades/APIRespuestas.cs
@@ -0,0 +1,21 @@
+﻿using Api_Agendate_App.Constantes;
+
+namespace Api_Agendate_App.Utilidades
+{
+    public class APIRespuestas
+    {
+        public int codigo {  get; set; }
+        public string mensaje { get; set; }
+
+        public APIRespuestas()
+        {
+           this.codigo = 0;
+           this.mensaje = string.Empty;
+        }
+
+        public void ObtenerMensaje(int p_codigo)
+        {
+            this.mensaje = ConstantesDeErrores.DevolverMensaje(p_codigo);
+        }
+    }
+}

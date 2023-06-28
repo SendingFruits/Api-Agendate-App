1cb68416e669e33cb030a2644188756588644c18
Khutlu <kevinmiranda621@gmail.com>
2023-06-27 23:59:37 -0300

    feat: Interfases

---
+++ Repositorio/Interfases/IUsuario.cs
@@ -0,0 +1,15 @@
+﻿
+using Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.IRepositorio
+{
+    public interface IUsuario : IRepositorio<Usuario>
+    {
+        Task<Usuario> Actualizar(Usuario entidad);
+    }
+}

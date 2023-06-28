d17e7130278b0de717984f7502d6e3a1781a3595
Khutlu <kevinmiranda621@gmail.com>
2023-06-27 23:59:50 -0300

    feat: Repositories

---
+++ Repositorio/EntidadesDeRepositorio/ClienteRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Logic.Data;
+using Repositorio.IRepositorio;
+
+namespace Repositorio
+{
+    public class ClienteRepositorio : Repositorio<ClienteRepositorio>, IClienteRepositorio
+    {
+        private readonly DataContext _db;
+        public ClienteRepositorio(DataContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public async Task<ClienteRepositorio> Actualizar(ClienteRepositorio entidad)
+        {
+            throw new NotImplementedException();
+            /*_db.clientes.Add(entidad);
+            await _db.SaveChangesAsync();
+            return entidad;*/
+        }
+    }
+}

d4ea397a55278363498b8115a1d97a147cf0eff2
Khutlu <kevinmiranda621@gmail.com>
2023-09-26 11:54:27 -0300

    feat: Mejoras y nuevos mensajes de errores

--- Api-Agendate-App/Constantes/ConstantesDeErrores.cs
+++ Api-Agendate-App/Constantes/ConstantesDeErrores.cs
@@ -10,10 +10,13 @@
         public const int ErrorEntidadInexistente = 1003;
 
 
-        #region Clientes 1101 al 1200-> ...
+        #region Clientes 1101 al 1200 -> ...
         public const int ErrorClienteConDocumentoExistente = 1101;
         #endregion
 
+        #region Empresas 1201 al 1300 -> ...
+        public const int ErrorEmpresaConDocumentoExistente = 1201;
+        #endregion
         #region Errores Generales 1901 al 2000 -> ...
         public const int ErrorClaveViejaIngresadaConfirmarVacia = 1901;
         public const int ErrorClaveNuevaIngresadaConfirmarVacia = 1902;
@@ -30,21 +33,20 @@
         // Del 3001 al 4000 
         #region Errores Generales 
         public const int ErrorCredencialesIncorrectas = 3001;
-
+        public const int ErrorContraseniaViejaNoCoincide = 3002;
         #endregion
 
         public static string DevolverMensaje(int codigoError)
         {
             string mensaje = "";
 
-            /// Errores por entidad (Singular)
+            if (codigoError == 0) return "Exito";
+
+            #region Errores por entidad (Singular) --> ...
             if (codigoError >= 1001 && codigoError <= 2000)
             {
                 switch (codigoError)
                 {
-                    case 0:
-                        mensaje = "Exito";
-                        break;
                     case ErrorEntidadExistente:
                         mensaje = "La entidad a crear ya existe en el sistema";
                         break;
@@ -56,7 +58,9 @@
                         break;
                 }
             }
-            //Errores de Clientes
+            #endregion
+
+            #region Errores de Clientes --> ...
             else if (codigoError >= 1101 && codigoError <= 1200)
             {
                 switch (codigoError)
@@ -66,26 +70,39 @@
                         break;
                 }
             }
-            //Errores generales en singular
-            else if( codigoError >= 1901 && codigoError <= 2000)
+            #endregion
+
+            #region Errores de Empresas --> ...
+            else if (codigoError >= 1201 && codigoError <= 1300)
+            {
+                switch (codigoError)
+                {
+                    case ErrorEmpresaConDocumentoExistente:
+                        mensaje = "Ya existe una Empresa con el RUT ingresado";
+                        break;
+                }
+            }
+            #endregion
+            #region Errores generales en singular --> ...
+            else if ( codigoError >= 1901 && codigoError <= 2000)
             {
                 switch (codigoError)
                 {
                     case ErrorClaveViejaIngresadaConfirmarVacia:
                         mensaje = "La clave vieja ingresada no puede ser vacía";
                         break;
-                    case 
+                    case ErrorClaveNuevaIngresadaConfirmarVacia:
+                        mensaje = "La clave nueva ingresada no puede ser vacía";
+                        break;
                 }
 
             }
-            //Errores por entidades(Plural)
+            #endregion
+            #region Errores por entidades(Plural) --> ...
             else if  (codigoError >= 2001 && codigoError <= 3000)
             {
                 switch (codigoError)
                 {
-                    case 0:
-                        mensaje = "Exito";
-                        break;
                     case ErrorEntidadExistente:
                         mensaje = "Las entidades a crear ya existen en el sistema";
                         break;
@@ -97,7 +114,22 @@
                         break;
                 }
             }
-            
+            #endregion
+            #region Errores Generales --> ...
+            else if (codigoError >= 3001 && codigoError <= 4000)
+            {
+                switch (codigoError)
+                {
+                    case ErrorCredencialesIncorrectas:
+                        mensaje = "Las credenciales ingresadas no son correctas";
+                        break;
+                    case ErrorContraseniaViejaNoCoincide:
+                        mensaje = "La contraseña vieja ingresada no coincide con la del usuario";
+                        break;
+                }
+            }
+            #endregion
+
             return mensaje;
         }
     }

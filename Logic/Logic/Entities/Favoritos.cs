117857a049e963c840d1d1323c6eeab133ca1f0c
byteKAMM <kevinmiranda621@gmail.com>
2024-01-24 20:14:21 -0300

    feat: Nuevas entidades favoritos

---
+++ Logic/Logic/Entities/Favoritos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Entities
+{
+    public class Favoritos
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int? ClienteId { get; set; }
+        public virtual Clientes? Cliente { get; set; }
+
+        public int? ServicioId { get; set; }
+
+        public virtual Servicios? Servicio { get; set; }
+
+        public int? EmpresaId { get; set; }
+
+        public virtual Empresas? Empresa { get; set; }
+    }
+}
